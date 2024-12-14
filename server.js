849e0f32217ff044e99192dd34cfa798b9c1efba
DESKTOP-H746NHR\HP <aptechsheh@gmail.com>
2024-12-14 09:35:54 +0500

    I hate this

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ app.use(bodyParser.json());
 app.use(express.json());
 const cors = require('cors');
 app.use(cors({
-  origin: 'https://trackfolio-194c2.web.app/',  // Allow this origin
-  methods: ['GET', 'POST', 'PUT', 'OPTIONS'],  // Allow preflight OPTIONS request
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true  // If your requests include cookies or authentication headers
+  origin: 'https://trackfolio-194c2.web.app',  
+  methods: ['GET', 'POST', 'PUT', 'OPTIONS'],  
+  allowedHeaders: ['Content-Type', 'Authorization'],  
+  credentials: true  
 }));
+app.options('*', cors());
 
 
 
